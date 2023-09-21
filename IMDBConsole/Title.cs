bc0c9ab480b6bd73b4ab9459871f8554f7cd9f10
miksezea <mikkel.eilskov@gmail.com>
2023-09-21 15:28:25 +0200

    Fully re-done

--- IMDBConsole/Title.cs
+++ IMDBConsole/Title.cs
@@ -8,13 +8,25 @@ namespace IMDBConsole
 {
     public class Title
     {
-        public string Tconst { get; set; }
-        public string TitleType { get; set; }
-        public string PrimaryTitle { get; set; }
-        public string OriginalTitle { get; set; }
-        public bool IsAdult { get; set; }
-        public int? StartYear { get; set; }
-        public int? EndYear { get; set; }
-        public int? RuntimeMinutes { get; set; }
+        public string tconst { get; set; }
+        public string titleType { get; set; }
+        public string primaryTitle { get; set; }
+        public string originalTitle { get; set; }
+        public bool isAdult { get; set; }
+        public int? startYear { get; set; }
+        public int? endYear { get; set; }
+        public int? runtimeMinutes { get; set; }
+
+        public Title(string tconst, string titleType, string primaryTitle, string originalTitle, bool isAdult, int? startYear, int? endYear, int? runtimeMinutes)
+        {
+            this.tconst = tconst;
+            this.titleType = titleType;
+            this.primaryTitle = primaryTitle;
+            this.originalTitle = originalTitle;
+            this.isAdult = isAdult;
+            this.startYear = startYear;
+            this.endYear = endYear;
+            this.runtimeMinutes = runtimeMinutes;
+        }
     }
 }
